6d0a4ba4ee4731fa1f3638c9ef255420b581d6e1
jemus42 <jemus42@quantenbrot.de>
2016-02-20 11:25:49 +0100

    Cleanup

--- dataprep.R
+++ dataprep.R
@@ -8,21 +8,38 @@ library(tidyr)
 library(DT)
 source("helpers.R")
 
-incomparable <- get_initial_stats("theincomparable",
-                                  show_title = "The Incomparable") %>%
+
+#### Okay ####
+# Output seems to be okay
+
+incomparable <- get_initial_stats("theincomparable", show_title = "The Incomparable") %>%
                   get_podcast_stats()
-robot        <- get_initial_stats("robot",
-                                  show_title = "Robot or Not") %>%
+robot        <- get_initial_stats("robot", show_title = "Robot or Not") %>%
                   get_podcast_stats()
-teevee       <- get_initial_stats("teevee",
-                                  show_title = "TeeVee") %>%
+gameshow     <- get_initial_stats("gameshow", show_title = "Game Show") %>%
+                get_podcast_stats()
+
+#### Needs work ####
+# Something is not right, but it's more or less usable
+teevee       <- get_initial_stats("teevee", show_title = "TeeVee") %>%
                   get_teevee_stats()
 
+#### Experimental ####
+# Testing
+
+#### Saving files locally ####
 saveRDS(incomparable, "data/incomparable.rds")
 saveRDS(robot,        "data/robot.rds")
 saveRDS(teevee,       "data/teevee.rds")
+saveRDS(gameshow,     "data/gameshow.rds")
 
-incomparable_master <- bind_rows(incomparable, robot, teevee)
+#### Binding the good datasets to a master dataset ####
+incomparable_master <- bind_rows(incomparable, robot, gameshow)
+saveRDS(incomparable_master, "data/incomparable_master.rds")
 
+#### Quick check ####
 incomparable_master %>% group_by(podcast) %>%
-  summarize(episodes = length(unique(number)))
+  summarize(episodes = length(unique(number)),
+            guests   = length(guest),
+            guests_unique = length(unique(guest)),
+            hours    = round(sum(duration)/60, 2))
