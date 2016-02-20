2bb09e471372c3806931569282ec632b36e0f09d
jemus42 <jemus42@quantenbrot.de>
2016-02-20 06:55:52 +0100

    Data cleaning somewhat finished

--- dataprep.R
+++ dataprep.R
@@ -5,27 +5,28 @@ library(dplyr)
 library(stringr)
 library(lubridate)
 library(tidyr)
+library(DT)
 source("helpers.R")
 
 incomparable <- "https://www.theincomparable.com/theincomparable/stats.txt" %>%
                   read_csv(col_names = F, trim_ws = T, col_types = c()) %>%
-                  set_colnames(c("number", "date", "duration", "title", "host", paste0("guest", 1:4)))
+                  set_colnames(c("number", "date", "duration", "title", "host", paste0("guest", 1:(ncol(.)-5))))
 
 incomparable %<>%
   filter(!is.na(number)) %>%
   mutate(duration = parse_duration(duration),  # Duration in minutes
          date = dmy(date),
          host_first = str_extract(host, "^\\w*\\s\\w*"),
-         host_second = str_replace(host, "^.*with\\s", "")) %>%
-  select(number, date, duration, title, host_first, host_second, everything())
+         host_second = str_replace(host, "^.*with\\s", ""),
+         host = host_first) %>%
+  separate(host_second, c("guest_1", "guest_2"), sep = " and ") %>%
+  separate(guest1, c("guest1_1", "guest1_2"), sep = " and ") %>%
+  separate(guest2, c("guest2_1", "guest2_2"), sep = " and ") %>%
+  separate(guest3, c("guest3_1", "guest3_2"), sep = " and ") %>%
+  separate(guest4, c("guest4_1", "guest4_2"), sep = " and ") %>%
+  gather(position, guest, contains("guest")) %>%
+  filter(!is.na(guest)) %>%
+  select(number, date, duration, title, host, guest, position) %>%
+  arrange(desc(number))
 
-
-#### Experimental plots for testing ####
-
-ggplot(data = incomparable, aes(x = reorder(host_first, duration), y = duration)) +
-  geom_boxplot() +
-  coord_flip() +
-  labs(title = "The Incomparable Podcast\nShow Duration by Primary Host",
-       x = "Host (first listed)", y = "Duration (min)") +
-  theme_bw()
-
+saveRDS(incomparable, "data/incomparable.rds")
