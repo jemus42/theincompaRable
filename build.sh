4a2e4f516843ea635b119e9579b0068abfefedce
jemus42 <jemus42@quantenbrot.de>
2016-03-01 16:35:15 +0100

    Tweak build script, which is pointless by now

--- build.sh
+++ build.sh
@@ -1,16 +1,5 @@
 #! /bin/zsh
 
-PATH=$PATH:/Users/Lukas/repos/jemus42/syncbin/bin/
-PROJECT_HOME='/Users/Lukas/repos/incomparable/'
 
-cd analyses
+Rscript -e "source('render_local.R')"
 
-for file in $(ls *Rmd); do
-  render $file;
-done
-
-echo "Pushing…"
-
-rsync -avz *.html *_files -e ssh strato:"/srv/stats.jemu.name/theincomparable/"
-
-echo "Done!"
