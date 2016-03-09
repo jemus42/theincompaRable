17d09155bec88ce3d613037016045d772cc48c28
jemus42 <jemus42@quantenbrot.de>
2016-03-09 21:03:10 +0100

    Add function to gather relay.fm data

--- helpers.R
+++ helpers.R
@@ -272,3 +272,44 @@ get_podcast_segment_episodes <- function(){
   return(ret)
 }
 
+#### Relay.fm ####
+parse_relay_feed <- function(url = "https://www.relay.fm/master/feed"){
+  feed <- read_html(url)
+
+  titles <- feed %>% html_nodes("title") %>%
+    html_text() %>%
+    magrittr::extract(-1)
+
+  number <- titles %>%
+    str_extract("\\d+:") %>%
+    str_replace(":", "") %>%
+    as.numeric()
+
+  show <- titles %>%
+    str_extract("^.*\\d") %>%
+    str_replace_all("\\s\\d+.*$", "")
+
+  durations <- feed %>%
+    html_nodes("duration") %>%
+    html_text() %>%
+    as.numeric() %>%
+    divide_by(60)
+
+  pubdate <- feed %>% html_nodes("pubdate") %>%
+    html_text() %>%
+    str_replace("^.{5}", "") %>%
+    lubridate::parse_date_time("%d %b %Y %H:%M:%S", tz = "GMT")
+  pubdate <- pubdate[-1]
+
+  people <- feed %>%
+    html_nodes("author") %>%
+    html_text() %>%
+    str_replace_all(" and ", ", ") %>%
+    magrittr::extract(-1)
+
+  df <- data_frame(number = number, podcast = show,
+                   title = titles, duration = durations, date = pubdate, people = people) %>%
+    mutate(month = month(date, label = T, abbr = T),
+           year  = year(date))
+  return(df)
+}
